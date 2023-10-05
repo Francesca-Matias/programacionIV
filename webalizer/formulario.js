c2693c5c0fdf47a037d9038afb1d40869c1bfa7f
JimenaGuevara <jimenaguevara28@gmail.com>
2023-10-05 19:46:21 -0300

    ordenado

--- webalizer/formulario.js
+++ webalizer/formulario.js
@@ -1,6 +1,6 @@
 const form = document.getElementById("loginForm");
 
-form.addEventListener("submit", function (e){
+form.addEventListener("submit", function (e) {
     e.preventDefault()
 
     const username = document.getElementById("username").value;
@@ -8,20 +8,19 @@ form.addEventListener("submit", function (e){
     //constante que contiene los caracteres especiales 
     const caracteresEspeciales = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
 
-    if(username.length == 0 || username.length == null || !caracteresEspeciales.test(username)) {
+    if (username.length == 0 || username.length == null || !caracteresEspeciales.test(username)) {
         alert("El nombre no es valido")
         return;
     }
-    
+
 
     const password = document.getElementById("password").value;
 
-    if(password.length < 8 ||password.length == 0 || password.length == null){
+    if (password.length < 8 || password.length == 0 || password.length == null) {
         alert("La contraseña no es valida");
         return;
-    } 
+    }
 
     console.log("username:", username);
     console.log("password:", password);
 });
-    
